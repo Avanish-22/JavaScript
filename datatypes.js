3f29578b9abd3a0ba4c3f77a61b5a9dd34c77f57
AVANISH R ANAND <avanishsinha38@gmail.com>
2026-05-01 02:15:44 +0530

    datatypes folder

--- datatypes.js
+++ datatypes.js
@@ -1,6 +1,22 @@
 "use strict";//treat all JS code as newr version
 // alert(3+3)//we are using node js and not browser
-console.log(3+3)//code readability should be high
-console.log("Avanish")
+// console.log(3+3)//code readability should be high
+// console.log("Avanish")
+
+let name="Avanish"
+let age=22
+let isloggedin=false
+
+// number=> 2 to the power 53
+// BigInt
+// string=>" "
+// boolean=>true/false
+// null=>standalone value 
+// undefined=>
+// symbol=>output
+//object
+console.log(typeof undefined);
+console.log(typeof null);
+
 
 
