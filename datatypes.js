fb719ad9ad283d93eed2051ba5b2f5c140d7f4c9
AVANISH R ANAND <avanishsinha38@gmail.com>
2026-05-01 02:10:17 +0530

    added some new files

---
+++ datatypes.js
@@ -0,0 +1,6 @@
+"use strict";//treat all JS code as newr version
+// alert(3+3)//we are using node js and not browser
+console.log(3+3)//code readability should be high
+console.log("Avanish")
+
+
---
+++ new.js
@@ -0,0 +1,19 @@
+// console.log("This is a mobile app")
+// console.log("1"+"1")
+// console.log(1+1)
+// console.log("1"+2)
+// console.log(2+"1")
+const accountId=16546
+let accountEmail="avanishsinha38@gmail.com"
+var accountPassword="12345"
+let accountCity;
+
+// accountId=2//not allowed
+accountEmail="avr@gmail.com"
+accountPassword="1212"
+
+console.log(accountId);
+/*
+prefer ot to use var because of issue in block scope and functional scope
+*/
+console.table([accountId, accountEmail ,accountPassword, accountCity])
